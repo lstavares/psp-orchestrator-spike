f62afdaa7ce5a74f2e2448a2acd56e13fb0fd252
Leandro Tavares <lstavares@gmail.com>
2026-06-12 17:21:07 +0000

    chore: adicionar .gitignore e injetar dependência de provedores no RoutingEngine

--- src/routing/routing.module.ts
+++ src/routing/routing.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RoutingEngine } from './routing.engine';
+import { DEFAULT_ROUTING_PROVIDERS } from './routing-provider.config';
 
 @Module({
-  providers: [RoutingEngine],
+  providers: [
+    RoutingEngine,
+    {
+      provide: 'ROUTING_PROVIDERS',
+      useValue: DEFAULT_ROUTING_PROVIDERS,
+    },
+  ],
   exports: [RoutingEngine],
 })
 export class RoutingModule {}
